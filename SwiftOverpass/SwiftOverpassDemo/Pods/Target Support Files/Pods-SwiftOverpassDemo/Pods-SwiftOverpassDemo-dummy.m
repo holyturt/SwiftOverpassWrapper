b0be558dacdc9eabf1c44d9ed73f60df508ce0a3
kame <holyturt@gmail.com>
2017-12-04 23:46:51 +0900

    Revert "Add podfile for demo project"
    This reverts commit 704b7ed83c45c3cca6cd11a3b8a197ea7373ca83.

--- SwiftOverpass/SwiftOverpassDemo/Pods/Target Support Files/Pods-SwiftOverpassDemo/Pods-SwiftOverpassDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftOverpassDemo : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftOverpassDemo
-@end
--- SwiftOverpass/SwiftOverpassDemo/Pods/Target Support Files/SwiftOverpass/SwiftOverpass-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftOverpass : NSObject
-@end
-@implementation PodsDummy_SwiftOverpass
-@end
