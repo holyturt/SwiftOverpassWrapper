e19c9e50a317a93e31841ac099076200a43b72a4
wtimme <wtimme@users.noreply.github.com>
2018-06-15 23:41:22 +0200

    Move reading and parsing relations from XML test file to a dedicated method
    This allows for this common code to be reused.

--- SwiftOverpassTests/Extensions/OverpassRelation_AEXMLElementTestCase.swift
+++ SwiftOverpassTests/Extensions/OverpassRelation_AEXMLElementTestCase.swift
@@ -15,15 +15,7 @@ import Alamofire
 class OverpassRelation_AEXMLElementTestCase: XCTestCase {
     
     func testInitWithXMLElementShouldParseSingleRelationXMLFile() {
-        guard let xmlElement = xmlRootElementInFile("SingleRelation") else {
-            XCTFail("Unable to load the test XML element from file.")
-            return
-        }
-        
-        let response = OverpassResponse(response: DataResponse<String>(request: nil, response: nil, data: Data(), result: Result<String>.success("")),
-                                        requestQuery: "")
-        
-        guard let relation = OverpassRelation(xmlElement: xmlElement, response: response) else {
+        guard let relation = singleRelationFromXMLFile("SingleRelation") else {
             XCTFail("The XML should properly initialize the model.")
             return
         }
@@ -43,6 +35,20 @@ class OverpassRelation_AEXMLElementTestCase: XCTestCase {
         XCTAssertEqual(relation.tags["addr:housenumber"], "16")
         XCTAssertEqual(relation.tags["addr:city"], "Hamburg")
         
+    }
+    
+    // MARK: Helper
+    
+    private func singleRelationFromXMLFile(_ name: String) -> OverpassRelation? {
+        guard let xmlElement = xmlRootElementInFile(name) else {
+            XCTFail("Unable to load the test XML element from file.")
+            return nil
+        }
+        
+        let response = OverpassResponse(response: DataResponse<String>(request: nil, response: nil, data: Data(), result: Result<String>.success("")),
+                                        requestQuery: "")
+        
+        return OverpassRelation(xmlElement: xmlElement, response: response)
     }
     
 }
