3e1ea9ea43b65b6c8e42818c0d289eaa860b93be
wtimme <wtimme@users.noreply.github.com>
2018-07-18 00:42:08 +0200

    Add test for creating a `OverpassResponse` from a string
    The two tests showcase that the parser is able to properly parse the responses
    from both Overpass as well as the OpenStreetMap API.

---
+++ SwiftOverpassTests/Models/OverpassResponseTestCase.swift
@@ -0,0 +1,61 @@
+//
+//  OverpassResponseTestCase.swift
+//  SwiftOverpassTests
+//
+//  Created by Wolfgang Timme on 7/18/18.
+//  Copyright © 2018 Sho Kamei. All rights reserved.
+//
+
+import XCTest
+
+import SwiftOverpass
+
+class OverpassResponseTestCase: XCTestCase {
+    
+    func testInitWithXMLStringOfOverpassResponseShouldParseTheXML() {
+        let xmlString = stringFromXMLFile("ShortenedResponseFromOverpass")
+        let requestQuery = "some query"
+        
+        do {
+            let response = try OverpassResponse(xml: xmlString,
+                                                requestQuery: requestQuery)
+            
+            XCTAssertEqual(response.nodes?.count, 1)
+            XCTAssertEqual(response.nodes?.first?.id, 266318045)
+            
+            XCTAssertEqual(response.ways?.count, 1)
+            XCTAssertEqual(response.ways?.first?.id, 107305183)
+            
+            XCTAssertEqual(response.relations?.count, 1)
+            XCTAssertEqual(response.relations?.first?.id, 124410)
+            
+            XCTAssertEqual(response.requestQuery, requestQuery)
+        } catch {
+            XCTFail("Failed to create response: \(error)")
+        }
+    }
+    
+    func testInitWithXMLStringOfOpenStreetMapAPIResponseShouldParseTheXML() {
+        let xmlString = stringFromXMLFile("ShortenedResponseFromOpenStreetMapAPI")
+        let requestQuery = "some query"
+        
+        do {
+            let response = try OverpassResponse(xml: xmlString,
+                                                requestQuery: requestQuery)
+            
+            XCTAssertEqual(response.nodes?.count, 1)
+            XCTAssertEqual(response.nodes?.first?.id, 378292)
+            
+            XCTAssertEqual(response.ways?.count, 1)
+            XCTAssertEqual(response.ways?.first?.id, 524734)
+            
+            XCTAssertEqual(response.relations?.count, 1)
+            XCTAssertEqual(response.relations?.first?.id, 1978434)
+            
+            XCTAssertEqual(response.requestQuery, requestQuery)
+        } catch {
+            XCTFail("Failed to create response: \(error)")
+        }
+    }
+    
+}
